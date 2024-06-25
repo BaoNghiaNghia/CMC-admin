65770e7af1c5103c64a9fc41685bf8b72c84e7aa
BaoNghiaNghia <baonghia.kht@gmail.com>
2024-06-25 17:06:57 +0700

    init source

---
+++ resources/assets/api/auth/endpoints.js
@@ -0,0 +1,7 @@
+"use strict";
+
+export const LOGIN_ENDPOINT = 'api/v1/users/login';
+export const UPDATE_PASSWORD_ENDPOINT = 'api/v1/users/password';
+export const DETAIL_PROFILE_ENDPOINT = 'api/v1/users/profile';
+export const UPDATE_PROFILE_ENDPOINT = 'api/v1/users/profile';
+export const REGISTER_ENDPOINT = 'api/v1/users/register';
